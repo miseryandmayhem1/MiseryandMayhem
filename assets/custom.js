c7b2f063e24946da9e1d601e530c71e4176a3240
al b <al.b@mac.com>
2025-09-17 10:28:16 -0400

    new dawnbreaker structures

--- assets/custom.js
+++ assets/custom.js
@@ -10,18 +10,16 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.querySelector(".header");
-  const isHomePage = window.location.pathname === "/"; // Check if on the home page
-  const mobileThreshold = 5; // Mobile scroll threshold in pixels
-  const desktopThreshold = 100; // Desktop scroll threshold in pixels
+  const isHomePage = window.location.pathname === "/";
+  const mobileThreshold = 5;
+  const desktopThreshold = 100;
 
-  // Set initial header state
   if (!isHomePage) {
-    header.classList.add("is-sticky"); // Default to black for non-home pages
+    header.classList.add("is-sticky");
   }
 
-  // Handle scroll behavior
   window.addEventListener("scroll", function () {
-    if (!isHomePage) return; // Only apply scrolling behavior on the home page
+    if (!isHomePage) return;
 
     const threshold =
       window.innerWidth <= 768 ? mobileThreshold : desktopThreshold;
